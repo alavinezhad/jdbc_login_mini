3e884ae299b61b239645bd1ed4de7f97a6d4892e
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-06 08:20:21 +0330

    add getter and setter

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -22,4 +22,44 @@ public class User {
         this.username = username;
         this.password = password;
     }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
