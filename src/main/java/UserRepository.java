84cd4857b801561e136c9f5975c45cc5f47d3b51
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-06 15:05:21 +0330

    add findByUsername method

--- src/main/java/UserRepository.java
+++ src/main/java/UserRepository.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserRepository {
@@ -21,5 +22,24 @@ public class UserRepository {
         preparedStatement.setString(4, user.getPassword());
 
         return preparedStatement.executeUpdate();
+    }
+    public User findByUsername(String username) throws SQLException {
+        Connection connection = jdbcConnection.getConnection();
+
+        String findUsername = "SELECT * FROM users WHERE username = ?;";
+        PreparedStatement preparedStatement = connection.prepareStatement(findUsername);
+        preparedStatement.setString(1, username);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String getUsername = resultSet.getString("username");
+            String password = resultSet.getString("password");
+            int id = resultSet.getInt("id");
+
+            return new User(id, firstName, lastName, getUsername, password);
+        }
+        else
+            return null;
     }
 }
